8952d4654a569d30b2ec0907b7b451d8b31aa12f
Hoang Anh Dinh Trinh <hoang.anh.dinh.trinh@gmail.com>
2015-07-06 14:32:08 +1200

    rail app treebook scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'f6bb391aaafcaec784eac1a3e07c8c22bb37e81941020b6d49a5fdccdc7326ade1eec20d409d10496b087a4c97e8d65cc4e158a8610ad99cdc2232940643c2c2'
